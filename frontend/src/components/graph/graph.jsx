9eb5e6af3afa027156a3152578b0f9505c286db5
Chris Bram <cpbram2011@gmail.com>
2020-12-24 19:36:15 -0600

    add react graphjs 2

--- frontend/src/components/graph/graph.jsx
+++ frontend/src/components/graph/graph.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Chart from 'chart.js';
-
-
-
+import { Doughnut } from 'react-chartjs-2';
+ 
 export default class Graph extends React.Component {
     constructor(props){
         super(props);
@@ -13,49 +11,44 @@ export default class Graph extends React.Component {
     render ()  {
         if (!this.props.data) return null;
         let keys = [0,0,0,0,0,0,0,0,0,0,0,0];
+        let modes = [0,0]
         this.props.data.forEach(x => {
             if (!x) {
             } else {
                 keys[x.key] += 1;
+                
             }
         });
         window.keys = keys;
-        var ctx = document.getElementById('Chart').getContext("2d");
-        
-        const chartParams = { //Mode
-            type: 'doughnut',
-            data: {
-                labels: ['C', 'Db', 'D', 'Eb', 'E', 'F','Gb','G','Ab','A','Bb','B'],
-                datasets: [{
-                    data: keys,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [],
-                    borderWidth: 1
-                }]
-            },
-            options: {}
+        const mydata = {
+            labels: ['Ab','A','Bb','B','C','Db','D','Eb','E','F','Gb','G'],
+            datasets: [{
+                data: keys,
+                backgroundColor: [
+                    '#e6194B',
+                    '#f58231',
+                    '#ffe119',
+                    '#bfef45',
+                    '#3cb44b',
+                    '#469990',
+                    '#42d4f4',
+                    '#4363d8',
+                    '#000075',
+                    '#911eb4',
+                    '#f032e6',
+                    '#b20035',
+                  ],
+            }]
         };
-        var myChart = new Chart(ctx, chartParams);
-        myChart.destroy()
-        myChart = new Chart(ctx, chartParams);
 
         return (
             <>
-               
-
+            <Doughnut
+                data={mydata}
+                width={100}
+                height={50}
+                options={{ maintainAspectRatio: false }}
+                />
             </>
         )
     }
