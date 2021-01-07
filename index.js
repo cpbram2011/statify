a046787a989ff5d15c4c8b1892379a7c0c34cbea
Chris Bram <cpbram2011@gmail.com>
2021-01-07 17:54:53 -0600

    add keys files

--- index.js
+++ index.js
@@ -15,4 +15,9 @@ app.get('/', (req, res) => {
 })
 
 
-// app.listen(8080)
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+  }
