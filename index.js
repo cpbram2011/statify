36e243b777418bb5de5302d02e3923cb304c2748
Chris Bram <cpbram2011@gmail.com>
2021-01-07 17:56:39 -0600

    minor tweak

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.use(bodyParser.json());
 
 app.use(auth)
 
-app.get('/', (req, res) => {
-    res.send('nice job')
-})
+// app.get('/', (req, res) => {
+//     res.send('nice job')
+// })
 
 
 if (process.env.NODE_ENV === 'production') {
