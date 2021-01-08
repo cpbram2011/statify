ca5813ddd831e2dfb15990b63832113081cf5882
Chris Bram <cpbram2011@gmail.com>
2021-01-07 19:45:24 -0600

    minor tweak

--- index.js
+++ index.js
@@ -15,8 +15,9 @@ app.use(auth)
 // })
 
 
-    app.use(express.static('frontend/public'));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
     app.get('/', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'frontend', 'public', 'index.html'));
+      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
     })
-  
+  }
